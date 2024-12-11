b5c9162dc8b325dd4721a4549063978cb4d825af
Documenter.jl <documenter@juliadocs.github.io>
2024-12-11 13:46:43 +0000

    build based on 7488b3e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ModelContextProtocol","category":"page"},{"location":"#ModelContextProtocol","page":"Home","title":"ModelContextProtocol","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ModelContextProtocol.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ModelContextProtocol]","category":"page"}]
+}
